71849282d1c31b17572badf561fb9f887458a8b6
Ahmad Syarifuddin <oden.psht@gmail.com>
2025-10-29 20:43:41 +0700

    Create Menu Object

---
+++ Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+};
+
+class Menu {
+
+    Object[][] daftarMenu = {
+        {"Nasi Goreng", 10000, "Makanan"},
+        {"Mie Ayam", 12000, "Makanan"},
+        {"Ayam Bakar", 15000, "Makanan"},
+        {"Bakso", 20000, "Makanan"},
+        {"Es Teh", 5000, "Minuman"},
+        {"Es Jeruk", 6000, "Minuman"},
+        {"Es Susu", 8000, "Minuman"},
+        {"Jus Alpukat", 10000, "Minuman"}
+    };
+}
